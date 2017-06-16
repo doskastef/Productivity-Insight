3d509c1e6fa092dc7f8b2a1c4a0cd36d67e77541
delicmakaveli <delic665@gmail.com>
2017-06-17 00:14:03 +0200

    added signup gui

--- app/src/main/java/eetc/com/productivityinsight/activity/SplashActivity.java
+++ app/src/main/java/eetc/com/productivityinsight/activity/SplashActivity.java
@@ -1,6 +1,9 @@
 package eetc.com.productivityinsight.activity;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
